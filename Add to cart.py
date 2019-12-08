9a3f279f5c22af0db82ccec56455ca258cc37a70
vladp <vladpirlog@ymail.com>
2019-12-08 23:13:56 +0200

    Add to cart

--- Add to cart.py
+++ Add to cart.py
@@ -18,8 +18,9 @@ driver.implicitly_wait(13)
 
 driver.find_element_by_id("emg-input-autosuggest").send_keys("monitor")
 driver.find_element_by_xpath('//*[@id="emg-category-menu-icon"]').click()
-driver.implicitly_wait(3)
+driver.implicitly_wait(10)
+driver.execute_script("window.scrollTo(0, 780)")
 
 driver.find_element_by_xpath('//*[@id="card_grid"]/div[1]/div/div/div[3]/div[3]/form/button').click()
-driver.find_element_by_xpath('/html/body/div[13]/div/div/div[1]/button/i').click()
-driver.find_element_by_xpath('//*[@id="my_cart"]/span[2]').click()
+driver.find_element_by_xpath('/html/body/div[12]/div/div/div[2]/div/div[3]/a').click()
+
