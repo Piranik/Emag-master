0a38223bd62e344462711b2f4828ede0e0c5e168
vladp <vladpirlog@ymail.com>
2019-12-09 03:41:11 +0200

    Project finished

--- Register.py
+++ Register.py
@@ -10,13 +10,13 @@ driver.maximize_window()
 #def emag_register():
 driver.get("https://www.emag.ro/user/login?ref=hdr_signup_btn")
 
-driver.find_element_by_id("email").send_keys("kxp51892@bcaoo.com")
+driver.find_element_by_id("email").send_keys("kxp51892@bcao.com")
 driver.find_element_by_xpath("/html/body/form/div[4]/div/button").click()
 
 driver.implicitly_wait(3)
 driver.find_element_by_id("r_name").send_keys("vlad")
-driver.find_element_by_id("r_password").send_keys("password123")
-driver.find_element_by_id("r_password_confirmation").send_keys("password123")
+driver.find_element_by_id("r_password").send_keys("password123@@@")
+driver.find_element_by_id("r_password_confirmation").send_keys("password123@@@")
 driver.implicitly_wait(3)
 
 driver.find_element_by_xpath("/html/body/form/div[6]").click()
@@ -27,8 +27,6 @@ actions = ActionChains(driver)
 actions.move_by_offset(802,550).click().perform()
 
 driver.find_element_by_xpath("/html/body/form/div[7]/div/button").click()
-
-#I don't know how to pass robot check
 
 #for i in range(20):
     #try:
